d75946b8483115abab63b730744ffa4d7c98223b
Abhishek Mazumder <ABHISHEKMAZUMDER72@gmail.com>
2025-01-28 23:01:41 +0530

    Silly Error With State Function In Api Fixed

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
-import game.GameView;
-import game.GameAPI;
+import api.GameAPI;
+import view.GameView;
 
 public class Main {
     public static void main(String[] args) {
+
+        // TEST---
+
+        // int[] board = {1, 0, 0, 
+        //                0, 0, 0,
+        //                0, 0, 0};
         
+        // System.out.println(GameAPI.getMove(board, -1));
+
+        // EXPECT 4
     }
 }
--- api/GameAPI.java
+++ api/GameAPI.java
@@ -5,15 +5,15 @@ public class GameAPI {
     public static final int X = 1; // Maximizing Player
     public static final int O = -1; // Minimizing Player
     public static final int DRAW = 0;
-    public static final int EMPYT = 0;
+    public static final int EMPTY = 0;
 
-    public int getMove(int[] board, int turn) {
+    public static int getMove(int[] board, int turn) {
         int bestMove = -1, bestScore = (turn == X)? Integer.MIN_VALUE : Integer.MAX_VALUE;
         for (int move=0; move<9; move++) {
-            if (board[move] == EMPYT) {
+            if (board[move] == EMPTY) {
                 board[move] = turn;
                 int score = evaluate(board, -turn, 0);
-                board[move] = EMPYT;
+                board[move] = EMPTY;
                 if (turn == X && score > bestScore) {
                     bestScore = score;
                     bestMove = move;
@@ -26,28 +26,28 @@ public class GameAPI {
         return bestMove;
     }
 
-    public int evaluate(int[] board, int turn, int depth) {
-        if (state(board) != DRAW) return (-depth*state(board));
+    public static int evaluate(int[] board, int turn, int depth) {
+        if (state(board) != DRAW) return ((10-depth)*state(board));
         int bestScore = (turn == X)? Integer.MIN_VALUE : Integer.MAX_VALUE;
         int moveCount = 0;
         for (int move=0; move<9; move++) {
-            if (board[move] == EMPYT) {
+            if (board[move] == EMPTY) {
                 moveCount++;
                 board[move] = turn;
                 int score = evaluate(board, -turn, depth + 1);
-                board[move] = EMPYT;
+                board[move] = EMPTY;
                 if (turn == X && score > bestScore)
-                    score = bestScore;
+                    bestScore = score;
                 else if (turn == O && score < bestScore) 
-                    score = bestScore;
+                    bestScore = score;
             }
         }
         return (moveCount == 0)? DRAW : bestScore;
     }
 
-    public int state(int[] board) {
+    public static int state(int[] board) {
         int[][] board2D = new int[3][3];
-        for (int i=0; i<3; i++) 
+        for (int i=0; i<9; i++) 
             board2D[i/3][i%3] = board[i];
 
         if (board2D[0][0] == board2D[1][1] && board2D[1][1] == board2D[2][2])
@@ -56,14 +56,14 @@ public class GameAPI {
         if (board2D[0][2] == board2D[1][1] && board2D[1][1] == board2D[2][0])
             return board2D[1][1];
         
-        for (int row=0; row<3; row++)
-            if (board2D[row][0] == board2D[row][1] && board2D[row][1] == board2D[row][2])
-                return board2D[row][1];
-        
-        for (int col=0; col<3; col++) 
-            if (board2D[0][col] == board2D[1][col] && board2D[1][col] == board2D[2][col])
-                return board2D[1][col];
+        for (int pos=0; pos<3; pos++) {
+            if (board2D[pos][0] == board2D[pos][1] && board2D[pos][1] == board2D[pos][2])
+                return board2D[pos][1];
+            if (board2D[0][pos] == board2D[1][pos] && board2D[1][pos] == board2D[2][pos])
+                return board2D[1][pos];
 
+        }
+        
         return DRAW;
     }
 }
